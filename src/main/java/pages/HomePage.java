fedce71522109d40cf002c0af371a7a34f9d6f61
bhaumikgohel <bhaumik.gohel@gmail.com>
2025-08-20 18:40:32 +0530

    Update HomePage

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,14 +1,14 @@
 package pages;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import javax.swing.text.Utilities;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.asserts.SoftAssert;
-
 
 import Utilities.Utils;
 import base.TestBase;
